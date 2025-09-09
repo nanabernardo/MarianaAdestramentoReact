ed04bdbe539530a2eec5e12dc47797070fc3b6cc
nanabernardo <nanabernardo@gmail.com>
2025-09-09 20:44:15 -0300

    chore: add placeholder text to Home page

--- MarianaAdestramentoReact/src/pages/Home.jsx
+++ MarianaAdestramentoReact/src/pages/Home.jsx
@@ -4,10 +4,10 @@ function Home() {
       <h1 className="text-5xl font-bold text-highlight mb-4">
         Mariana Bernardo Adestramento
       </h1>
-      <h2 className="text-2xl mb-6">Seu melhor amigo com melhor qualidade de vida</h2>
-      <p className="text-highlight font-semibold">
-        Se a cor laranja (highlight) aparecer, Tailwind está funcionando!
-      </p>
+      <h2 className="text-2xl mb-6">
+        Seu melhor amigo com melhor qualidade de vida
+      </h2>
+      <p>Lugar do texto com apresentação da página</p>
     </section>
   );
 }
