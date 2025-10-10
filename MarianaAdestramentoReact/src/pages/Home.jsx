b10a2cac1ad87e353779cae8604cb784b39669c1
nanabernardo <nanabernardo@gmail.com>
2025-10-10 18:04:45 -0300

    feat: increase side images while maintaining responsiveness on the Home page

--- MarianaAdestramentoReact/src/pages/Home.jsx
+++ MarianaAdestramentoReact/src/pages/Home.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import ResponsiveImage from "../components/ResponsiveImage";
 import BelaDora from "../assets/Bela_Dora_2011.jpg";
 import LukePortao from "../assets/IMG-20230725-WA0006.jpg";
-import DoraBelaPp from "../assets/Dora_Bela_Pp.jpg";
 
 const Home = () => {
   return (
     <section className="min-h-screen relative p-6 text-white">
-      {/* grid principal */}
-      <div className="grid grid-cols-1 md:grid-cols-[200px_1fr_200px] gap-6 items-start">
+      {/* conteiner principal */}
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-center gap-8 max-w-7xl mx-auto">
         {/* imagem esquerda (somente desktop) */}
-        <div className="hidden md:block mt-48">
+        <div className="hidden md:flex justify-center flex-shrink-0">
           <ResponsiveImage
             src={BelaDora}
             alt="Bela e Dora"
             aspect="1/1"
-            className="rounded-xl shadow-md w-48"
+            className="rounded-xl shadow-md max-w-[200px] w-full"
           />
         </div>
 
         {/* texto principal */}
-        <div className="max-w-4xl mx-auto mt-0 text-center">
+        <div className="flex-1 max-w-4xl mx-auto mt-0 text-center">
           <h1 className="text-5xl font-bold text-highlight mb-4">
             Mariana Bernardo Adestramento
           </h1>
@@ -66,12 +65,12 @@ const Home = () => {
         </div>
 
         {/* Imagem direita (somente desktop) */}
-        <div className="hidden md:block mt-48">
+        <div className="hidden md:flex justify-center flex-shrink-0">
           <ResponsiveImage
             src={LukePortao}
             alt="Dora e Bela"
             aspect="1/1"
-            className="rounded-xl shadow-md w-48"
+            className="rounded-xl shadow-md max-w-[200px] w-full"
           />
         </div>
       </div>
@@ -81,13 +80,13 @@ const Home = () => {
           src={BelaDora}
           alt="Bela e Dora"
           aspect="1/1"
-          className="rounded-xl shadow-md w-48"
+          className="rounded-xl shadow-md w-70"
         />
         <ResponsiveImage
           src={LukePortao}
           alt="Dora e Bela"
           aspect="1/1"
-          className="rounded-xl shadow-md w-48"
+          className="rounded-xl shadow-md w-70"
         />
       </div>
     </section>
